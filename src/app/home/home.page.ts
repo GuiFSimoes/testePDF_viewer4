3841ddaf85608e660c8d0fdd57fcad1afa91d4ea
Guilherme Simões <guifsimoes@gmail.com>
2019-03-21 18:15:06 -0300

    ultimos ajustes...

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import '@teamhive/pdf-viewer';
-import { File } from '@ionic-native/file/ngx';
-import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+/* import { File } from '@ionic-native/file/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx'; */
 
 @Component({
   selector: 'app-home',
@@ -10,29 +10,28 @@ import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 })
 export class HomePage {
 
+  private corsLink = 'https://cors-anywhere.herokuapp.com/';
+
   // tslint:disable-next-line: max-line-length
   private urlPDFFirebase = 'https://firebasestorage.googleapis.com/v0/b/studiopilates-a1b2.appspot.com/o/artigo.pdf?alt=media&token=53ffb50a-d248-4793-8633-b59bd7495111';
   private urlFile = 'file:///android_asset/www/assets/docs/documento.pdf';
-  private urlFileGit = 'file:///android_asset/www/assets/docs/documento.pdf';
+  private urlFilePDF = 'http://www.ufjf.br/revistaveredas/files/2009/11/ARTIGO-Maira-Avelar-e-Janaina-Rabelo.pdf';
   private mimeType = 'application/pdf';
 
   public urlLocal = '';
   public exibir = false;
+  public linkInput = '';
 
-  constructor(private file: File, private transfer: FileTransfer) { }
+  constructor(/* private file: File, private transfer: FileTransfer */) { }
 
   abrirPDF() {
-    const filePathSave = this.file.dataDirectory + 'artigo_nuvem.pdf';
-    this.exibir = true;
-    this.urlLocal = '';
 
-    const trans = this.transfer.create();
-    trans.download(this.urlPDFFirebase, filePathSave)
-      .then((result) => {
-        const arq = result.toURL();
-      }).catch((err) => {
-        console.log('falha: ', err);
-      });
+    this.exibir = true;
+    if (this.linkInput !== '') {
+      this.urlLocal = this.corsLink + this.linkInput;
+    } else {
+      this.urlLocal = this.corsLink + this.urlFilePDF;
+    }
   }
 
 }
